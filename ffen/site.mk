29d67d7ed559e20606529b4d73f0936f319db7d8
valcryst <mattes@air-raid.de>
2016-02-08 08:08:08 +0100

    Update to 0.8.3-2
    Added new check to the watchdog which restarts tunneldigger if no
    neighbors are present over the mesh-vpn interface.

--- ffen/site.mk
+++ ffen/site.mk
@@ -1,36 +1,35 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-core \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-hostname \
-        gluon-config-mode-tunneldigger \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-theme \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-wifi-config \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-next-node \
-        gluon-mesh-vpn-tunneldigger \
-        gluon-radvd \
-        gluon-setup-mode \
-        gluon-status-page \
-        gluon-ssid-changer \
-        gluon-migrate-vpn \
-        ffho-wifi-blackout-workaround \
-        ffho-autoupdater-wifi-fallback \
-        iwinfo \
-        iptables \
-        haveged
+    gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-luci-theme \
+	gluon-luci-private-wifi \
+	gluon-next-node \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+    gluon-migrate-vpn \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-ssid-changer \
+	haveged \
+	iptables \
+	iwinfo
 
-DEFAULT_GLUON_RELEASE := 0.8-L2TP
+DEFAULT_GLUON_RELEASE := 0.8.3-2
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
--- ffspr/site.mk
+++ ffspr/site.mk
@@ -1,34 +1,35 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-core \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-hostname \
-        gluon-config-mode-tunneldigger \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-portconfig \
-        gluon-luci-wifi-config \
-        gluon-luci-theme \
-        gluon-luci-private-wifi \
-        gluon-next-node \
-        gluon-mesh-vpn-tunneldigger \
-        gluon-migrate-vpn \
-        gluon-radvd \
-        gluon-setup-mode \
-        gluon-status-page \
-        gluon-ssid-changer \
-        haveged \
-        iptables \
-        iwinfo
+    gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-luci-theme \
+	gluon-luci-private-wifi \
+	gluon-next-node \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+    gluon-migrate-vpn \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-ssid-changer \
+	haveged \
+	iptables \
+	iwinfo
 
-DEFAULT_GLUON_RELEASE := 0.8.1
+DEFAULT_GLUON_RELEASE := 0.8.3-2
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
