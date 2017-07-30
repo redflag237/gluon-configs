7f60236b411970f3c453adf439fc5b97c2e473a0
marcel <marcel@freifunk-wetter.de>
2017-07-30 19:04:04 +0200

    Update FW 0.9.4 beta

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
 # gluon site configs for l2tp
 
-Additional repos used for Gluon 2016.2.6 
-
-* https://github.com/freifunk-en/ffen-packages
-* https://github.com/ffrl/ffrl-packages
-* https://github.com/freifunk-kiel/ffki-packages
-* https://github.com/freifunk-ffm/packages
-
-Change Log Gluon 2016.2.6 FW 0.9.1 / 0.9.1.1
-
-* ibss & 802.11s mesh activated (to deactivate ibss in next firmware) 
-* prefix4 removed
-* 1 more ntp server added
-* basic_rate & supported_rates added
-* new sign key added for marcel
-* autoupdater mirror updated
-* roles added but not in use in the backend at the moment
-* config mode geo location show altitude set to false
-* config mode owner obligatory set to true
-* config mode ppa added
-* gluon-ebtables-segment-mld added
-* ffffm-keep-radio-channel added
-* GLUON_ATH10K_MESH ?= 11s added
-
-Change Log Gluon 2016.2.6 FW 0.9.2
-* ibss mesh deactivated
-* autoupdater branch experimental removed
-* gluon-quickfix added
-
-Change Log Gluon 2016.2.6 FW 0.9.3 / 0.9.3.1
-* gluon-quickfix removed
-* own script added to make the nodes more stable
-
-Planned Changes
-* banner 
-* max. clients on wifi patch
-* ppa as text in config mode
+Change Log Gluon 2017.1.1 FW 0.9.4 beta
+* first config uploaded
--- ffept/site.mk
+++ ffept/site.mk
@@ -1,42 +1,131 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
-        gluon-respondd \
-        gluon-autoupdater \
-	gluon-config-mode-ppa\
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-core \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-hostname \
-        gluon-config-mode-tunneldigger \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-segment-mld \
-        gluon-luci-theme \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-wifi-config \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-	gluon-luci-node-role \
-        gluon-next-node \
-        gluon-mesh-vpn-tunneldigger \
-        gluon-tunneldigger-watchdog \
-	ffffm-keep-radio-channel \
-        ffen-ath9k-broken-wifi-workaround \
-        gluon-radvd \
-        gluon-setup-mode \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged \
-        ffen-ssidchanger
+	gluon-mesh-batman-adv-14 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+        gluon-ebtables-segment-mld \
+	gluon-web-admin \
+        gluon-web-network \
+	gluon-web-autoupdater \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	haveged
 
-DEFAULT_GLUON_RELEASE := 0.9.3
-# DEFAULT_GLUON_RELEASE := 0.9.3+exp$(shell date '+%Y%m%d')
+# support the USB stack
+USB_PACKAGES_BASIC := \
+	kmod-usb-core \
+	kmod-usb2
 
+# FAT32 Support for USB
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	kmod-fs-ext4 \
+	kmod-fs-vfat \
+	kmod-usb-storage  \
+	kmod-usb-storage-extras  \
+	blkid  \
+	swap-utils  \
+	kmod-nls-cp1250  \
+	kmod-nls-cp1251  \
+	kmod-nls-cp437  \
+	kmod-nls-cp775  \
+	kmod-nls-cp850  \
+	kmod-nls-cp852  \
+	kmod-nls-cp866  \
+	kmod-nls-iso8859-1  \
+	kmod-nls-iso8859-13  \
+	kmod-nls-iso8859-15  \
+	kmod-nls-iso8859-2  \
+	kmod-nls-koi8r  \
+	kmod-nls-utf8
+
+USB_PACKAGES_NET := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-ncm \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx \
+	kmod-mii \
+	kmod-nls-base
+
+TOOLS_PACKAGES := \
+	joe \
+	tcpdump \
+	vnstat \
+	iperf \
+	iperf3 \
+	socat \
+	usbutils
+
+
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-usb-hid \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(TOOLS_PACKAGES)
+endif
+
+#ar71xx-generic
+GLUON_DIR505A1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_TLWR842_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_TLWR1043_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_TLWR2543_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_TLWDR4300_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_WNDR3700_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_WRT160NL_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+# GLUON_DIR825B1_SITE_PACKAGES := kmod-usb-core kmod-usb2 tcpdump vnstat iperf usbutils joe kmod-mii kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net kmod-nls-base # too big
+GLUON_GLINET_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_WZRHPG450H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_WZRHPAG300H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_ARCHERC7_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+
+#mpc85xx-generic
+GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+
+DEFAULT_GLUON_RELEASE := lede-exp-$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
-GLUON_REGION ?= eu
+
 GLUON_LANGS ?= en de
+
 GLUON_ATH10K_MESH ?= 11s
+
+GLUON_REGION := eu
